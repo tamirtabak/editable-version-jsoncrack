6718f68f18d4aceeceda28ec9c0345a24a18d4ef
Tamir Tabak <tamirtabak@gmail.com>
2026-06-22 11:24:43 +0300

    Adding start.bat file to launch automatically the local app

---
+++ start.bat
@@ -0,0 +1,14 @@
+bat@echo off
+title jsoncrack.com Runner
+cd /d "D:\PycharmProjects\jsoncrack.com"
+
+echo Installing/updating dependencies...
+call pnpm install
+
+echo Starting browser...
+start http://localhost:3000/
+
+echo Starting jsoncrack.com on http://localhost:3000/
+call pnpm dev:www
+
+pause
